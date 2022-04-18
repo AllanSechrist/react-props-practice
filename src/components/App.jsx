85d24a49e2dccc3dac405f7437d3d134a807fe37
AllanSechrist <sechristallan@gmail.com>
2022-04-19 07:53:42 +0900

    create MakeCard function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,22 @@ import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
 
+function MakeCard(contact) {
+  return <Card 
+    key={contact.id}
+    id={contact.id}
+    name={contact.name}
+    imgURL={contact.imgURL}
+    phone={contact.phone}
+    email={contact.email}
+  />
+}
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      {contacts.map((contact) => <Card 
-        name={contact.name}
-        imgURL={contact.imgURL}
-        phone={contact.phone}
-        email={contact.email}
-      />)}
+      {contacts.map(MakeCard)}
     </div>
   );
 }
